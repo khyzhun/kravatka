649db536afddfac882e3037a5a1205cd6b9a0fa7
yura <yura.zb.ua@gmail.com>
2023-10-18 20:25:27 +0300

    implementation SignInScreen

---
+++ app/src/main/java/com/khyzhun/kravatka/pages/sign_in/SignInViewModel.kt
@@ -0,0 +1,10 @@
+package com.khyzhun.kravatka.pages.sign_in
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignInViewModel @Inject constructor() : ViewModel() {
+
+}
