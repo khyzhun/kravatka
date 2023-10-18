bc5cbf3492a8b27a8fc229778e0e445446a224a4
khyzhun <sasha.khyzhun@gmail.com>
2023-10-18 23:28:54 +0300

    code polishing;

--- app/src/main/java/com/khyzhun/kravatka/navigation/Routes.kt
+++ app/src/main/java/com/khyzhun/kravatka/navigation/Routes.kt
@@ -1,9 +1,35 @@
 package com.khyzhun.kravatka.navigation
 
 sealed class Routes(val route: String) {
+
+    /**
+     * Entry point
+     */
     object Splash : Routes("splash")
+
+    /**
+     * Welcome screen for new users
+     */
     object Welcome : Routes("welcome")
+
+    /**
+     * Authentication
+     */
     object SignIn : Routes("sign_in")
     object SignUp : Routes("sign_up")
-    object Main : Routes("main")
+
+    /**
+     * Main scree Nested graph
+     */
+
+    object Feed : Routes("feed")
+    object Favourites : Routes("favourites")
+    object Cart : Routes("cart")
+    object Profile : Routes("profile")
+
+    /**
+     * Profile graph
+     */
+
+
 }
--- app/src/main/java/com/khyzhun/kravatka/pages/MainActivity.kt
+++ app/src/main/java/com/khyzhun/kravatka/pages/MainActivity.kt
@@ -3,18 +3,7 @@ package com.khyzhun.kravatka.pages
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.khyzhun.kravatka.navigation.Routes
-import com.khyzhun.kravatka.pages.splash.SplashScreen
-import com.khyzhun.kravatka.pages.splash.SplashViewModel
-import com.khyzhun.kravatka.pages.welcome.WelcomeScreen
-import com.khyzhun.kravatka.pages.welcome.WelcomeViewModel
+import com.khyzhun.kravatka.navigation.RootAppNavigation
 import com.khyzhun.kravatka.ui.theme.KravatkaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,73 +18,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
-
-@Composable
-fun RootAppNavigation(
-    modifier: Modifier = Modifier,
-    navController: NavHostController = rememberNavController(),
-    startDestination: String = Routes.Splash.route,
-) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = startDestination,
-    ) {
-
-        composable(Routes.Splash.route) {
-            val viewModel = hiltViewModel<SplashViewModel>()
-            SplashScreen(
-                viewModel = viewModel,
-                onNavigationNext = {
-                    navController.navigate(route = Routes.Welcome.route) {
-                        popUpTo(0)
-                    }
-                }
-            )
-        }
-        composable(Routes.Welcome.route) {
-            val viewModel = hiltViewModel<WelcomeViewModel>()
-            WelcomeScreen(
-                viewModel = viewModel,
-                onNavigateToSignIn = {
-                     navController.navigate(route = Routes.SignIn.route)
-                },
-                onNavigateToSignUp = {
-                     navController.navigate(route = Routes.SignUp.route)
-                },
-            )
-
-        }
-        composable(Routes.SignIn.route) {
-            // TBD.
-        }
-        composable(Routes.SignUp.route) {
-            // TBD.
-        }
-        composable(Routes.Main.route) {
-            // TBD.
-        }
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
