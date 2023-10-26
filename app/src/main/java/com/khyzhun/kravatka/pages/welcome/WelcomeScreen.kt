8f2cfb201a544e0310f70b046522365901e3c553
yura <yura.zb.ua@gmail.com>
2023-10-26 18:30:40 +0300

    implementation WelcomeScreen

--- app/src/main/java/com/khyzhun/kravatka/pages/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/khyzhun/kravatka/pages/welcome/WelcomeScreen.kt
@@ -1,18 +1,35 @@
 package com.khyzhun.kravatka.pages.welcome
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.khyzhun.kravatka.R
+import com.khyzhun.kravatka.core.components.PrimaryButton
 import com.khyzhun.kravatka.ui.theme.PurpleGrey40
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,15 +40,18 @@ fun WelcomeScreen(
     onNavigateToSignUp: () -> Unit,
 ) {
     Scaffold(
-        topBar = {
-            TopBar()
-        },
         content = {
             WelcomeScreenContent(
-                modifier = Modifier.padding(it),
-                onNavigateToSignIn = onNavigateToSignIn,
-                onNavigateToSignUp = onNavigateToSignUp
+                modifier = Modifier.padding(it)
             )
+        },
+        bottomBar = {
+            Box(modifier = Modifier.padding(16.dp)) {
+                PrimaryButton(
+                    text = stringResource(id = R.string.next),
+                    onClick = onNavigateToSignIn
+                )
+            }
         }
     )
 }
@@ -51,26 +71,49 @@ private fun TopBar() {
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun WelcomeScreenContent(
-    modifier: Modifier,
-    onNavigateToSignIn: () -> Unit,
-    onNavigateToSignUp: () -> Unit
+    modifier: Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Navigate to Sign In",
-            modifier = Modifier.clickable {
-                onNavigateToSignIn.invoke()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val pageCount = 5
+        val pagerState = rememberPagerState(initialPage = 0)
+        HorizontalPager(
+            modifier = modifier.border(1.dp, Color.Black),
+            pageCount = pageCount,
+            state = pagerState
+        ) {
+            Box(modifier.fillMaxSize()) {
+                Text(
+                    text = it.toString()
+                )
             }
-        )
-        Text(
-            text = "Navigate to Sign Up",
-            modifier = Modifier.clickable {
-                onNavigateToSignUp.invoke()
-            }
-        )
+        }
+        Row(
+            Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.Gray else Color.Gray.copy(alpha = 0.5f)
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(8.dp)
 
+                )
+            }
+        }
     }
 }
 
@@ -80,11 +123,7 @@ private fun WelcomeScreenContent(
 private fun WelcomeScreenContentPreview() {
     WelcomeScreenContent(
         modifier = Modifier,
-        onNavigateToSignIn = { /*TODO*/ },
-        onNavigateToSignUp = { /*TODO*/ }
     )
-
-
 }
 
 
