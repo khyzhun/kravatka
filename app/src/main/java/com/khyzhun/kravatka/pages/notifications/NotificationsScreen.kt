71766ee86e0dd952817c0ea3953c97ad9ea5b45d
yura <yura.zb.ua@gmail.com>
2023-10-26 16:25:29 +0300

    implementation NotificationsScreen

--- app/src/main/java/com/khyzhun/kravatka/pages/notifications/NotificationsScreen.kt
+++ app/src/main/java/com/khyzhun/kravatka/pages/notifications/NotificationsScreen.kt
@@ -5,17 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.khyzhun.kravatka.R
@@ -33,11 +35,11 @@ import com.khyzhun.kravatka.core.components.TopBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NotificationsScreen() {
+fun NotificationsScreen(onBackClick: () -> Unit) {
     Scaffold(topBar = {
         TopBar(
             title = stringResource(id = R.string.notifications),
-            onBackClick = { }
+            onBackClick = onBackClick
         )
     }
     ) {
@@ -47,16 +49,18 @@ fun NotificationsScreen() {
 
 @Composable
 fun NotificationsScreenContent(modifier: Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        NotificationsItem(
-            title = "You got discount for this week",
-            message = "We have a good news. You got discount for this week for -10%"
-        )
-        Divider(
-            thickness = 1.dp,
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
-        NotificationsItem(title = "title", message = "message message message message", isNew = true)
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        itemsIndexed(listMessages) { index, item ->
+            NotificationsItem(
+                title = item.title,
+                message = item.message,
+                isNew = item.isNew
+            )
+            if (index < listMessages.lastIndex) ItemDivider()
+        }
     }
 }
 
@@ -76,25 +80,20 @@ fun NotificationsItem(
                 color = if (isNew) Color(0xFFF3F3F3) else Color(0xFFFFFFFF)
             )
             .padding(8.dp)
-
-
-//            .border(1.dp, Color.Black)
-
     ) {
         Row(
             modifier = modifier.fillMaxWidth(),
             verticalAlignment = Alignment.Top,
             horizontalArrangement = Arrangement.End
         ) {
-            if(isNew) {
-                Icon(
-                    Icons.Filled.Notifications,
-                    contentDescription = "Check mark"
+            if (isNew) {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_fiber_manual_record_24),
+                    contentDescription = stringResource(id = R.string.new_notifications),
+                    Modifier.size(16.dp)
                 )
             }
-
         }
-
         Row {
             Image(
                 painter = painterResource(R.drawable.ic_launcher_background),
@@ -117,9 +116,48 @@ fun NotificationsItem(
                     overflow = Ellipsis,
                     modifier = modifier.padding(top = 8.dp)
                 )
-
             }
         }
-
     }
 }
+
+@Composable
+private fun ItemDivider() {
+    Divider(
+        thickness = 1.dp,
+        modifier = Modifier.padding(vertical = 8.dp)
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun NotificationsScreenPreview() {
+    NotificationsScreen { TODO() }
+}
+
+
+data class MockMessage(
+    val title: String,
+    val message: String,
+    val isNew: Boolean
+)
+
+val listMessages = listOf(
+    MockMessage(
+        "You got discount for this week",
+        "We have a good news. You got discount for this week for -10%",
+        isNew = false
+    ),
+    MockMessage(
+        "You got discount for this week",
+        "We have a good news. You got discount for this week for -10%",
+        isNew = true
+    ),
+
+    MockMessage(
+        "You got discount for this week",
+        "We have a good news. You got discount for this week for -10%",
+        isNew = false
+    )
+)
